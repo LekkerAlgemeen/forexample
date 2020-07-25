423f16478451262f9ae16379d50add7fc4aea329
Tim Vos <tim@Tims-MBP.home>
2020-07-25 22:31:34 +0200

    Ninja level 9, exercise 2

---
+++ 9_2/9_2.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type human interface {
+	speak()
+}
+
+func main() {
+	p := person{
+		first: "James",
+		last:  "Bond",
+		age:   37,
+	}
+
+	saySomething(p)
+}
+
+func (p *person) speak() {
+	fmt.Println("Hi, my name is", p.first, p.last, "and my age is", p.age)
+}
+
+func saySomething(h human) {
+	h.speak()
+}
