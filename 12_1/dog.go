7e0f489c850d9e5120e71788b8489e6054891ff0
Tim Vos <tim@Tims-MBP.home>
2020-08-06 15:42:36 +0200

    Removed last exercise:

--- 12_1/dog.go
+++
@@ -1,8 +0,0 @@
-// package dog contains methods to convert dog years into human years
-package dog
-
-// function Years takes human years
-// and turns them into dog years (1 human year = 7 dog years)
-func Years(a int) int {
-	return a * 7
-}
